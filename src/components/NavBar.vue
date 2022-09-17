47703f6538947a815eba1e1469d1ab2e750c1bbc
victorv47 <victor.vela94@gmail.com>
2022-09-17 13:35:14 -0500

    Nav Bar creation and styling

---
+++ src/components/NavBar.vue
@@ -0,0 +1,34 @@
+<!-- eslint-disable prettier/prettier -->
+<script setup>
+  import { ref } from 'vue'
+  const brand = ref("🏢 Victor's Company Directory")
+</script>
+
+<template>
+  <nav>
+    <div class="wrapper">
+      <div class="brand">
+        <span class="brand-title">{{ brand }}</span>
+      </div>
+      <div class="menu">
+        <a href="#" class="menu-item">Departments</a>
+        <a href="#" class="menu-item">Settings</a>
+        <a href="#" class="menu-item">logout</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped lang="postcss">
+  nav {
+    @apply flex h-20 bg-slate-500 text-slate-100;
+    .wrapper {
+      @apply container mx-auto flex w-full items-center justify-between;
+      .brand {
+        &-title {
+          @apply text-2xl font-bold;
+        }
+      }
+    }
+  }
+</style>
