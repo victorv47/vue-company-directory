235a4aa0adc8c49cf03eb5daecded6d6de2a1c61
victorv47 <victor.vela94@gmail.com>
2022-11-27 01:13:06 -0600

    Fixewd Linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,18 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  extends: ['plugin:vue/vue3-recommended', 'eslint:recommended', 'prettier'],
-  plugins: ['prettier'],
+  extends: ['plugin:vue/vue3-recommended', 'eslint:recommended', '@vue/eslint-config-prettier'],
   parserOptions: {
     ecmaVersion: 'latest',
   },
   rules: {
-    'prettier/prettier': ['error'],
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: false,
+      },
+    ],
     'arrow-body-style': 'off',
     'prefer-arrow-callback': 'off',
   },
