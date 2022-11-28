63c2ee820c38a5cbca66c29fe8ef2a002985bbfc
victorv47 <victor.vela94@gmail.com>
2022-11-27 22:53:58 -0600

    Added Pagination API

--- src/components/MainPagination.vue
+++ src/components/MainPagination.vue
@@ -1,34 +1,43 @@
 <script setup>
-  import { ref } from 'vue'
-  const pages = ref(10)
-  const activePage = ref(4)
+  import useAPI from '@/composables/useAPI'
 
-  const prevPage = () => {
+  const { activePage, pages, getEmployees } = useAPI()
+
+  const prevPage = async () => {
     if (activePage.value > 1) {
       activePage.value--
+      await getEmployees()
     }
   }
-
-  const nextPage = () => {
+  const nextPage = async () => {
     if (activePage.value < pages.value) {
       activePage.value++
+      await getEmployees()
     }
+  }
+  const jumpPage = async (page) => {
+    activePage.value = page
+    await getEmployees()
   }
 </script>
 
 <template>
   <div class="pagination">
-    <button class="action" :disabled="activePage === 1" @click="prevPage">Prev</button>
+    <button class="action" :disabled="activePage === 1" @click="prevPage">
+      Prev
+    </button>
     <button
       v-for="page in pages"
       :key="page"
       class="page"
       :class="page === activePage ? 'active' : ''"
-      @click="activePage = page"
+      @click="jumpPage(page)"
     >
       {{ page }}
     </button>
-    <button class="action" :disabled="activePage === pages" @click="nextPage">Next</button>
+    <button class="action" :disabled="activePage === pages" @click="nextPage">
+      Next
+    </button>
   </div>
 </template>
 
@@ -41,7 +50,7 @@
     .page {
       @apply rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md hover:bg-slate-200;
       &.active {
-        @apply bg-blue-900 text-slate-100 hover:bg-blue-700;
+        @apply bg-yellow-700 text-slate-100 hover:bg-yellow-600;
       }
     }
   }
